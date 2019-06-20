9df2d79e5119c9b6ff8de5d860d3fa22373f44de
Adrian <ongadrian98@gmail.com>
2019-06-20 16:37:26 +0800

    Added start message

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 import telegram
+import os
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
 
-bot = telegram.Bot(token="842599326:AAH7j1KG0IUOeuHXBe13_HgDG2pFcfr0l6Q")
+bot = telegram.Bot(token=os.getenv("BOT_TOKEN", False))
+updater = Updater(token=os.getenv("BOT_TOKEN", False), use_context=True)
+dispatcher = updater.dispatcher
 
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 print(bot.get_me())
+updater.start_polling()
