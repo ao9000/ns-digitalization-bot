dbb43b4201ae1a95755a60279eecb0db433e3738
adrian <ongadrian98@gmail.com>
2021-04-07 00:11:55 +0800

    Updated start command

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
-import telegram
 import os
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
-bot = telegram.Bot(token=os.getenv("BOT_TOKEN", False))
-updater = Updater(token=os.getenv("BOT_TOKEN", False), use_context=True)
+# Define bot
+updater = Updater(token=os.getenv("bot_token"), use_context=True)
 dispatcher = updater.dispatcher
 
 
+# Commands
+# Start command
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode="MarkdownV2",
+                             text="*What can this bot do?*\n"
+                                  "This is a platform for all vehicle fault reporting\. Users can update the vehicle "
+                                  "management team about the fault they are facing with thier vehicle\. Vehicle "
+                                  "management team will then contact the user to solve thier issue\.")
 
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-print(bot.get_me())
-updater.start_polling()
+
+if __name__ == '__main__':
+    updater.start_polling()
