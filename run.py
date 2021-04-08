8b7cc901b9845c61b48e62f555b2fe44eebc3f12
adrian <ongadrian98@gmail.com>
2021-04-09 00:19:25 +0800

    Added conversation handler

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 import os
 import telegram
-from telegram.ext import CommandHandler, MessageHandler, Updater, Filters
-
-# Define bot
-updater = Updater(token=os.getenv("bot_token"), use_context=True)
-dispatcher = updater.dispatcher
+from telegram.ext import CommandHandler, MessageHandler, Updater, Filters, ConversationHandler
 
 
 # Commands
@@ -14,7 +10,7 @@ def start(update, context):
     choices = [
         [telegram.KeyboardButton("/Vehicle_physical_damage")]
     ]
-    keyboard_markup = telegram.ReplyKeyboardMarkup(choices)
+    keyboard_markup = telegram.ReplyKeyboardMarkup(choices, one_time_keyboard=True)
 
     # Send message
     context.bot.send_message(chat_id=update.effective_chat.id, parse_mode="MarkdownV2", reply_markup=keyboard_markup,
@@ -24,7 +20,41 @@ def start(update, context):
 
 
 start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+
+
+# Vehicle physical damage command
+# Conversation entry point
+def vehicle_physical_damage(update, context):
+    # Prompt user
+    update.message.reply_text("What physical damage did the vehicle sustain?")
+
+    return 1
+
+
+vehicle_physical_damage_handler = CommandHandler('Vehicle_physical_damage', vehicle_physical_damage)
+
+
+def get_physical_damage(update, context):
+    damage = update.message.text
+    context.user_data["physical_damage"] = damage
+
+    if len(damage) < 4:
+        update.message.reply_text("Please provide more details")
+        return vehicle_physical_damage(update, context)
+
+    update.message.reply_text("Alright, May I know your name?")
+
+    return 2
+
+
+def get_name(update, context):
+    name = update.message.text
+    context.user_data["name"] = name
+
+    print(context.user_data["physical_damage"])
+    print(context.user_data["name"])
+
+    return ConversationHandler.END
 
 
 # Error message for invalid commands
@@ -33,10 +63,31 @@ def error_message(update, context):
 
 
 error_handler = MessageHandler(Filters.command, error_message)
-dispatcher.add_handler(error_handler)
 
 
+def main():
+    # Define bot
+    updater = Updater(token=os.getenv("bot_token"), use_context=True)
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[vehicle_physical_damage_handler],
+        states={
+            1: [MessageHandler(Filters.text, get_physical_damage)],
+            2: [MessageHandler(Filters.text, get_name)]
+        },
+        fallbacks=[]
+    )
+
+    # Add handlers
+    dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(start_handler)
+    # dispatcher.add_handler(error_handler)
+
+    # Start bot, stop when interrupted
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
-    updater.start_polling()
+    main()
