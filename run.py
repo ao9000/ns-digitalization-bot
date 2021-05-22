87e4c6e7ec8db3b84f42945d963b4eede0915ee2
adrian <ongadrian98@gmail.com>
2021-05-23 01:03:43 +0800

    Added doc string for each function

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
+"""
+    Runs the infrastructure fault reporting bot on Telegram
+
+    Project's aim:
+    This bot aims to ease availability choke point faced when a single user is responsible for handling newly reported faults.
+    Acting like a middleman, the bot can then swiftly disseminate newly reported faults to the appropriate personnel.
+
+    Bot features:
+        1. 24/7 availability
+        2. Chat with multiple users simultaneously
+        3. Fault tracking
+        4. Near instantaneous dissemination of information
+
+    Bot commands:
+        1. /history
+        2. /start
+        3. /resolved {fault_id}
+
+    Requires an environment file with the following variables:
+        1. bot_token - API token of the bot, can be created via @BotFather
+        2. recipient_list - Telegram chat id for users who wants to be notified by the bot for new faults (Separated by comma for multiple users)
+"""
+
+# Import statements
 import os
 import re
 import telegram
@@ -30,6 +54,15 @@ class EnvironmentVariableError(Exception):
 # Helper functions
 # Formatting user data for display
 def display_user_details(update):
+    """
+    Returns the user details who was chatting with the bot in displaying format
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :return: type: str
+    Formatted user details (First name, last name & username)
+    """
     response = f'*Name:* [{escape_markdown(text=update.effective_user.first_name, version=2)} {escape_markdown(text=update.effective_user.last_name, version=2) if update.effective_user.last_name else ""}](tg://user?id={update.effective_user.id})'\
                f'{f", *Username:* [{escape_markdown(text=update.effective_user.username, version=2)}](https://t.me/{update.effective_user.username})" if update.effective_user.username else ""}'
     return response
@@ -37,6 +70,15 @@ def display_user_details(update):
 
 # Formatting user data for logging
 def get_user_details(update):
+    """
+    Returns the user details who was chatting with the bot in logging format
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :return: type: str
+    Formatted user details (First name, last name & username)
+    """
     response = f'UserID: {update.effective_user.id}, Name: {update.effective_user.first_name}'\
                f'{f" {update.effective_user.last_name}" if update.effective_user.last_name else ""}'\
                f'{f", Username: {update.effective_user.username}" if update.effective_user.username else ""}'
@@ -45,6 +87,16 @@ def get_user_details(update):
 
 # Get latest fault running number and returns a new fault number
 def get_fault_index(context):
+    """
+    Get the total amount of faults (resolved/active) and derive a new fault id for the newly reported fault
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: str
+    Number of the new fault id
+    """
+
     # Both dicts are not empty
     if all(context.bot_data[dict_name] for dict_name in ["active_history", "resolved_history"]):
         index = len(context.bot_data['active_history']) + len(context.bot_data['resolved_history'])
@@ -91,7 +143,14 @@ logging.info(f'{len(recipient_list)} recipients loaded')
 
 # Decorator for paginating replies
 def PaginationHandlerMeta(func):
+    """
+    Meta function
+    """
     def PaginationHandler(*args, **kwargs):
+        """
+        Splits large amounts of characters in a single message into multiple messages to avoid the max characters length for a single message
+        The max character per message is 4096
+        """
         # Define separator
         separator = "\n\n\n"
 
@@ -145,6 +204,20 @@ def PaginationHandlerMeta(func):
 # Commands
 # History command
 def history(update, context):
+    """
+    Entry point for history command in conversation handler, used for displaying fault history
+    /history command of the bot
+
+    Allows the user to select between either resolved or active faults. Then displays the respective faults in a list format
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: int
+    The id of the next state defined in conversation handler
+    """
     logging.info(f'{get_user_details(update)}, Action: /history')
 
     # Define keyboard choices
@@ -166,6 +239,18 @@ history_handler = CommandHandler('history', history, Filters.user(user_id=set(in
 
 @PaginationHandlerMeta
 def get_history_version(update, context):
+    """
+    Handles the user input, only accepts 'active' or 'resolved' and returns the respective dict
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: str or dict
+    Returns str if there is no fault history, else returns dict if there is at least 1 fault saved
+    """
     # Standardise user input
     history_version = update.message.text.lower()
 
@@ -190,6 +275,15 @@ def get_history_version(update, context):
 
 
 def mark_resolve_active_fault(update, context):
+    """
+    Mark an active fault as resolved
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+    """
     # Handle user input
     fault_id = context.args
 
@@ -240,6 +334,19 @@ mark_resolve_active_fault_handler = CommandHandler('resolved', mark_resolve_acti
 # Start command
 # Conversation entry point #1
 def start(update, context):
+    """
+    Entry point for conversation handler for fault reporting, used to submit a new fault
+    /start command of the bot
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: int
+    The id of the next state defined in conversation handler
+    """
     logging.info(f'{get_user_details(update)}, Action: /start')
 
     # Prompt user
@@ -252,6 +359,18 @@ new_fault_handler = CommandHandler('start', start)
 
 
 def get_type_of_fault(update, context):
+    """
+    Handles the user input for the type of fault, only accepts text with character limit between 4> and 500<
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: int
+    The id of the next state defined in conversation handler
+    """
     # Save user input
     type_of_fault = update.message.text
     context.user_data["type_of_fault"] = type_of_fault
@@ -265,6 +384,18 @@ def get_type_of_fault(update, context):
 
 
 def get_description_of_fault(update, context):
+    """
+    Handles the user input for the description of fault, only accepts text with character limit between 4> and 500<
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: int
+    The id of the next state defined in conversation handler
+    """
     # Save user input
     description_of_fault = update.message.text
     context.user_data["description_of_fault"] = description_of_fault
@@ -278,6 +409,20 @@ def get_description_of_fault(update, context):
 
 
 def get_location_of_fault(update, context):
+    """
+    Handles the user input for the location of fault, only accepts text with character limit between 4> and 500<
+
+    After that, constructs a fault summary message to allow the user to confirm all their inputs before sending to the respective personnel
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: int
+    The id of the next state defined in conversation handler
+    """
     # Save user input
     location_of_fault = update.message.text
     context.user_data["location_of_fault"] = location_of_fault
@@ -311,6 +456,20 @@ def get_location_of_fault(update, context):
 
 # Sending user information & damage details to Maintenance personnel
 def send_details_to_maintenance_clerks(update, context):
+    """
+    Handles the user input the confirmation of fault summary message
+
+    After that, sends the fault summary message including the submitters details to the respective personnel
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+
+    :return: type: int
+    The id of the next state defined in conversation handler
+    """
     # Standardise user input
     confirmation = update.message.text.lower()
 
@@ -360,6 +519,15 @@ def send_details_to_maintenance_clerks(update, context):
 # Error messages
 # Invalid command (General)
 def error_command_general(update, context):
+    """
+    Error message for when user input a invalid command outside of conversation handler states
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+    """
     logging.info(f'{get_user_details(update)}, Error: Invalid command (General)')
     update.message.reply_text("Invalid. Please provide a valid command")
     update.message.reply_text("Type /start to get started")
@@ -370,6 +538,15 @@ error_command_general_handler = MessageHandler(Filters.all, error_command_genera
 
 # User cancelled conversation
 def error_user_cancelled(update, context):
+    """
+    Error message for when the user input /exit
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+    """
     logging.info(f'{get_user_details(update)}, Action: /exit')
 
     # Exit conversation
@@ -384,12 +561,30 @@ def error_user_cancelled(update, context):
 
 # User insufficient input
 def error_insufficient_input(update, context):
+    """
+    Error message for when the user input lesser than 4 characters
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+    """
     logging.info(f'{get_user_details(update)}, Error: Insufficient information provided')
     update.message.reply_text("Invalid. Please provide more information")
     update.message.reply_text("Type /exit to cancel this conversation")
 
 
 def error_max_limit_input(update, context):
+    """
+    Error message for when the user input more than 500 characters
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+    """
     logging.info(f'{get_user_details(update)}, Error: Too many characters per message, must be <1k')
     update.message.reply_text("Exceeded character limit. Please the fault below 500 characters")
     update.message.reply_text("Type /exit to cancel this conversation")
@@ -397,12 +592,29 @@ def error_max_limit_input(update, context):
 
 # User invalid command (Conversational)
 def error_command_input(update, context):
+    """
+    Error message for when user input a invalid command inside of conversation handler states
+
+    :param update: type: telegram.update.Update
+    Object represents an incoming update.
+
+    :param context: type: telegram.ext.callbackcontext.CallbackContext
+    This is a context object passed to the callback called by telegram.ext.Handler or by the telegram.ext.Dispatcher in an error handler added by telegram.ext.Dispatcher.add_error_handler or to the callback of a telegram.ext.Job
+    """
     logging.info(f'{get_user_details(update)}, Error: Invalid command (Conversational)')
     update.message.reply_text("Invalid. Please provide a valid command")
     update.message.reply_text("Type /exit to cancel this conversation")
 
 
 def main():
+    """
+    Main function of the bot
+
+    Does the following:
+        1. Initialize the conversation handler and its states
+        2. Adds all message/command handlers
+        3. Starts the bot and keeps it running
+    """
     # Define conversation handler
     conv_handler = ConversationHandler(
         entry_points=[
